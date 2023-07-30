9f4abdd19f0587899cec7b3fa0e3f0e4d07dcf5d
JaeilJo <whwodlf97@gmail.com>
2023-07-30 19:18:32 +0900

    refactor : Add Carousel in SkillSection

--- src/components/Sections/SkillSection/index.tsx
+++ src/components/Sections/SkillSection/index.tsx
@@ -1,35 +1,71 @@
-import Skills from "@/utils/Skills";
-import { useState } from "react";
+import Skills from '@/utils/Skills';
+import { useState } from 'react';
 
-import SkillItem from "./SkillItem";
+import SkillItem from './SkillItem';
 
 function SkillSection() {
-  const [currentSkill, setCurrentSkill] = useState(0);
+    const [currentSkill, setCurrentSkill] = useState(1);
+    const cloneLast = { ...Skills[0], key: 9 };
+    const cloneFirst = { ...Skills[Skills.length - 1], key: 10 };
 
-  return (
-    <div className="flex w-screen h-screen max-sm:flex-col">
-      <div className="flex items-center justify-center w-full h-full text-9xl max-md:text-7xl">
-        Skill
-      </div>
-      <div className="relative flex items-center justify-center w-full h-full max-sm:items-start">
-        <div className="absolute z-30 flex w-full h-full bg-red-600 ">
-          <button className="w-full h-full bg-blue-300">HEllo</button>
-          <button className="w-full h-full">HEllo</button>
+    const [newArray, setNewArray] = useState([cloneFirst, ...Skills, cloneLast]);
+
+    const [animation, setAnimation] = useState(``);
+    const leftMove = [
+        '-translate-x-[0em]',
+        '-translate-x-[10em]',
+        '-translate-x-[20em]',
+        '-translate-x-[30em]',
+        '-translate-x-[40em]',
+        '-translate-x-[50em]',
+        '-translate-x-[60em]',
+        '-translate-x-[70em]',
+        '-translate-x-[80em]',
+    ];
+    const leftClick = () => {
+        if (currentSkill === 1) {
+            setTimeout(() => {
+                setCurrentSkill(9);
+            }, 100);
+        }
+        setCurrentSkill(currentSkill - 1);
+        setAnimation('animate-section_skill_animation');
+        setTimeout(() => {
+            setAnimation('');
+        }, 300);
+    };
+
+    const rightClick = () => {
+        if (currentSkill === Skills.length) {
+            setTimeout(() => {
+                setCurrentSkill(1);
+            }, 100);
+        }
+        setCurrentSkill(currentSkill + 1);
+        setAnimation('animate-section_skill_animation');
+        setTimeout(() => {
+            setAnimation('');
+        }, 300);
+    };
+
+    return (
+        <div className="flex w-screen h-screen max-sm:flex-col">
+            <div className="flex items-center justify-center w-full h-full text-9xl max-md:text-7xl">Skill</div>
+            <div className="relative flex items-center justify-center w-full h-full max-sm:items-start">
+                <div className="absolute z-30 flex w-full h-full ">
+                    <button className="w-full h-full " onClick={leftClick} />
+                    <button className="w-full h-full" onClick={rightClick} />
+                </div>
+                <div className="relative text-2xl lg:text-5xl md:text-4xl w-[10em] h-[10em]  overflow-hidden ">
+                    <ul className={`absolute flex h-full  ${leftMove[currentSkill]}`}>
+                        {newArray.map((skill) => (
+                            <SkillItem key={skill.key} title={skill.title} icon={skill.icon} animation={animation} />
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="relative text-2xl lg:text-5xl md:text-4xl w-[10em] h-[10em]  overflow-hidden ">
-          <ul className="absolute flex h-full -left-[0em] duration-200">
-            {Skills.map((skill) => (
-              <SkillItem
-                key={skill.key}
-                title={skill.title}
-                icon={skill.icon}
-              />
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SkillSection;
