3b3ecc7688fc0718002bb6f0ef845b509539f9b3
JaeilJo <whwodlf97@gmail.com>
2023-07-31 23:25:13 +0900

    refactor : add main onClick

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,21 @@
 import Sections from '@/utils/Sections';
 import Section from '../Section';
 import Footer from '../Footer';
+import { useNavigation } from '@/zustand/navigationStore';
+import { useEffect } from 'react';
 
 function Main() {
+    const [is_clicked, click] = useNavigation((state) => [state.is_clicked, state.click]);
+
     return (
-        <div className="absolute lg:text-3xl md:text-1xl  mt-[5em] w-screen  bg-neutral-50 dark:bg-neutral-900">
+        <div
+            className="absolute lg:text-3xl md:text-1xl  mt-[5em] w-screen  bg-neutral-50 dark:bg-neutral-900"
+            onClick={() => {
+                if (is_clicked) {
+                    click();
+                }
+            }}
+        >
             {Sections.map((section) => (
                 <Section key={section.key} id={section.id} content={section.content} />
             ))}
