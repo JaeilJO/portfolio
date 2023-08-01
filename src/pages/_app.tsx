1382878576b2a9beb49b196f433804c888443ee9
JaeilJO <whwodlf97@gmail.com>
2023-08-01 13:12:33 +0900

    feat : Add Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Oswald } from 'next/font/google';
-import { ThemeProvider } from 'next-themes';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Oswald } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
-const oswald = Oswald({ subsets: ['latin'] });
+const oswald = Oswald({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <ThemeProvider attribute="class">
-            <main className={oswald.className}>
-                <Component {...pageProps} />
-            </main>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider attribute="class">
+      <main className={oswald.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en" className="dark scroll-smooth">
-            <Head />
-            <body className="dark:bg-neutral-900 bg-neutral-50 ">
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en" className="dark scroll-smooth">
+      <Head>
+        <title>Jaeil Studio</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+      </Head>
+      <body className="dark:bg-neutral-900 bg-neutral-50 ">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
