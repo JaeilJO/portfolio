c08e0d514112fe702ddb442633567370dd1f0bce
JaeilJo <whwodlf97@gmail.com>
2023-08-01 15:54:40 +0900

    refactor : refactoring Section Size and Refactoring SkillSection

--- src/components/Sections/HomeSection/index.tsx
+++ src/components/Sections/HomeSection/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import CodeWrite from '/public/codewrite.svg';
+
 import HandWrite from '/public/handwrite.svg';
 import { useInView } from 'react-intersection-observer';
 import Image from 'next/image';
@@ -10,14 +10,16 @@ function HomeSection() {
     });
 
     return (
-        <div className="flex flex-col justify-between w-screen min-h-screen " ref={ref}>
+        <div className="flex flex-col justify-between w-screen h-screen " ref={ref}>
             <div className="relative flex flex-1 w-full overflow-hidden ">
-                <div className="relative w-full top-5  scale-[50%] dark:fill-neutral-50 fill-neutral-900">
-                    <HandWrite />
+                <div className="relative flex items-center justify-center w-full h-full dark:fill-neutral-50 fill-neutral-900">
+                    <div className="relative w-[50%]">
+                        <HandWrite />
+                    </div>
                 </div>
             </div>
 
-            <div className="relative flex flex-col items-center justify-start flex-1 gap-10 ">
+            <div className="relative flex flex-col items-center justify-start gap-10 ">
                 <div className="relative text-9xl max-sm:text-6xl max-md:text-7xl">A Front engineer</div>
                 <div
                     className={`relative  w-[80%] h-[2px]  bg-neutral-900 ${
--- src/components/Sections/SkillSection/Explanation/index.tsx
+++ src/components/Sections/SkillSection/Explanation/index.tsx
@@ -1,18 +1,20 @@
-import { BsFillCursorFill } from "react-icons/bs";
+import { BsFillCursorFill } from 'react-icons/bs';
 
 function Explanation() {
-  return (
-    <div className="absolute flex items-center justify-center w-full h-full text-3xl text-stone-50 dark:text-white">
-      <div className="relative w-full flex items-center justify-center max-sm:-top-[15%] h-[50%]  bg-black dark:bg-dark opacity-20 dark:opacity-10 rounded-md"></div>
-      <div className="absolute  max-sm:top-[35%] z-20 animate-section_skill_click_animation">
-        <BsFillCursorFill />
-      </div>
-      <div className="absolute top-[55%] max-sm:top-[45%] font-thin text-[0.8em] text-center ">
-        Click to see More
-      </div>
-      <div className="absolute top-[47%] z-10 left-[50%] rounded-full max-sm:top-[33%] border-2 border-stone-50 dark:border-white w-[1em] h-[1em]  animate-section_skill_click_round_animation" />
-    </div>
-  );
+    return (
+        <div className="absolute flex items-center justify-center w-full h-full lg:text-3xl md:text-xl text-neutral-700 dark:text-stone-50 ">
+            <div className="relative w-[50%]  h-[30%] max-sm:-top-[25%]">
+                <div className="absolute w-full h-full bg-black rounded-xl opacity-10" />
+                <div className="absolute top-[50%] left-[50%] -translate-x-2/4 -translate-y-2/4  animate-section_skill_click_animation">
+                    <BsFillCursorFill />
+                </div>
+                <div className="absolute  font-thin text-[0.8em] bottom-10 left-2/4 -translate-x-2/4  text-center ">
+                    Click to see More
+                </div>
+                <div className="absolute top-[50%] left-[50%] -translate-x-[10%] -translate-y-[60%] rounded-full  border-2 border-neutral-700 dark:border-neutral-50 w-[1em] h-[1em]  animate-section_skill_click_round_animation" />
+            </div>
+        </div>
+    );
 }
 
 export default Explanation;
