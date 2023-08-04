7585893b6ae125b59746dc886dc38b1378d9b4ec
JaeilJo <whwodlf97@gmail.com>
2023-08-05 06:15:16 +0900

    testing

--- src/components/CommonComponents/CommonLeftText.tsx
+++ src/components/CommonComponents/CommonLeftText.tsx
@@ -1,6 +1,6 @@
 function CommonLeftText({ title }: { title: string }) {
     return (
-        <div className="relative  flex items-center justify-center duration-200   w-full h-full  max-sm:-top-[100px]   max-sm:items-end text-9xl max-md:text-7xl  ">
+        <div className="relative flex items-center justify-center max-sm:h-[50%] w-full h-full duration-200 bg-red-400 max-sm:items-end text-9xl max-md:text-7xl ">
             <div className="relative bottom-10">{title}</div>
         </div>
     );
