eeefdd7c4ad1819fb6bb8adf0994c2a523827cf5
JaeilJo <whwodlf97@gmail.com>
2023-08-05 06:49:08 +0900

    testing

--- src/components/CommonComponents/CommonRightContentWrapper.tsx
+++ src/components/CommonComponents/CommonRightContentWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function CommonRightContentWrapper({ children }: { children: ReactNode }) {
     return (
-        <div className="relative flex items-center justify-center w-full h-full max-h-full bg-blue-400 max-sm:items-start max-w-screen">
+        <div className="relative flex items-center justify-center w-full h-full max-h-full max-sm:items-start max-w-screen">
             <div className="flex items-center justify-center w-full h-full max-sm:items-start ">
                 <div className="relative w-[80%] h-[50%] max-sm:h-full max-sm:w-full max-w-[500px] max-h-[700px] min-x-[400px] min-y-[200px] overflow-hidden ">
                     <div className="flex items-center justify-center w-full h-full max-sm:items-start">{children}</div>
