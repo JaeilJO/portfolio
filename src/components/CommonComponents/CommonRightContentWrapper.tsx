5cc5053ade829e22502af28e19ec978d44a7418e
JaeilJo <whwodlf97@gmail.com>
2023-08-05 05:11:03 +0900

    texting

--- src/components/CommonComponents/CommonRightContentWrapper.tsx
+++ src/components/CommonComponents/CommonRightContentWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function CommonRightContentWrapper({ children }: { children: ReactNode }) {
     return (
-        <div className=" relative flex items-center justify-center max-sm:-top-[100px] bg-red-200  w-full h-full   max-sm:items-start max-h-screen max-w-screen">
+        <div className=" relative flex items-center justify-center max-sm:-top-[100px]   w-full h-full   max-sm:items-start max-h-screen max-w-screen">
             <div className="flex items-center justify-center w-full h-full max-sm:items-start ">
                 <div className="relative w-[80%] h-[50%] max-sm:h-full max-sm:w-full max-w-[500px] max-h-[700px] min-x-[400px] min-y-[200px] overflow-hidden ">
                     <div className="flex items-center justify-center w-full h-full max-sm:items-start">{children}</div>
--- src/components/Sections/ContactSection/index.tsx
+++ src/components/Sections/ContactSection/index.tsx
@@ -1,6 +1,9 @@
 import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { useAlert } from '@/zustand/contactAlertStore';
+import CommonSectionBackground from '@/components/CommonComponents/CommonSectionBackground';
+import CommonLeftText from '@/components/CommonComponents/CommonLeftText';
+import CommonRightContentWrapper from '@/components/CommonComponents/CommonRightContentWrapper';
 
 function ContactSection() {
     const textarea = useRef<HTMLTextAreaElement>(null!);
@@ -40,10 +43,46 @@ function ContactSection() {
     };
 
     return (
-        <div className="flex ">
-            {/* <div className="flex items-center justify-center grow-[1]  max-sm:items-end text-9xl max-sm:text-6xl ">
-                <div className="relative max-sm:bottom-10">Contact</div>
-            </div> */}
+        <CommonSectionBackground>
+            <CommonLeftText title="Contact" />
+            <CommonRightContentWrapper>
+                <div className="w-full h-full ">
+                    <form
+                        className="flex flex-col justify-between w-full h-full max-sm:px-6 "
+                        ref={form}
+                        onSubmit={sendEmail}
+                    >
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="E-mail"
+                            className="p-3  rounded-none mt-1  text-[18px] font-thin border-b-2 bg-transparent border-stone-200  placeholder:font-light  focus:outline-none max-sm:text-[16px] placeholder:text-neutral-900 dark:placeholder:text-neutral-50 "
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            className="p-3  rounded-none mt-1  text-[18px] font-thin border-b-2 bg-transparent border-stone-200  placeholder:font-light  focus:outline-none max-sm:text-[16px] placeholder:text-neutral-900 dark:placeholder:text-neutral-50 "
+                            required
+                        />
+                        <textarea
+                            className="p-3  rounded-none max-h-[220px]  text-[18px] font-thin bg-transparent placeholder-opacity-0 border-b-2  placeholder:font-light focus:outline-none placeholder:text-neutral-900 dark:placeholder:text-neutral-50"
+                            ref={textarea}
+                            onChange={handleResizeHeight}
+                            rows={1}
+                            name={'message'}
+                            placeholder="Message"
+                            required
+                        />
+                        <input
+                            type="submit"
+                            value="Submit"
+                            className="p-3 text-3xl font-light duration-200 border-4 rounded-lg cursor-pointer border-neutral-50 dark:border-neutral-900 dark:bg-neutral-50 dark:text-neutral-900 bg-neutral-900 text-neutral-50 hover:bg-neutral-700 dark:hover:bg-neutral-400"
+                        />
+                    </form>
+                </div>
+            </CommonRightContentWrapper>
 
             {/* <div className="flex items-center justify-center grow-[1] text-2xl  max-sm:text-[1.2rem] max-sm:items-start px-10  ">
                 <form
@@ -83,7 +122,7 @@ function ContactSection() {
                     />
                 </form>
             </div> */}
-        </div>
+        </CommonSectionBackground>
     );
 }
 
